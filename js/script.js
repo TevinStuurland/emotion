6b0fe261fab1ba9774b5b7075a7e25b0ce20c2a7
TevinStuurland <tevin94@hotmail.com>
2017-10-18 16:46:46 +0200

    asdasd

--- js/script.js
+++ js/script.js
@@ -207,21 +207,22 @@ $("#response").text(data);
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
     
-      var data = canvas.toDataURL('image/png');
+      var data = canvas.toDataURL('image/png'),
+      test = dataURItoBlob( data )
 
          function snapPost () {
             let file = document.getElementById('photo').src.substring(23).replace(' ', '+');
             let Img = Base64Binary.decodeArrayBuffer(file);
             let ajax = new XMLHttpRequest(); 
             console.log('Photo 1 in case');
-            CallAPI(Img, apiUrl, apiKey)       
+            CallAPI(test, apiUrl, apiKey)       
            }
              function snapPost2 () {
             let file = document.getElementById('photo-2').src.substring(23).replace(' ', '+');
             let Img = Base64Binary.decodeArrayBuffer(file);
             let ajax = new XMLHttpRequest(); 
             console.log('Photo 1 in case');
-            CallAPI(Img, apiUrl, apiKey)       
+            CallAPI(test, apiUrl, apiKey)       
            }
       
       switch (count % 2) {
@@ -250,6 +251,26 @@ $("#response").text(data);
   // once loading is complete.
   window.addEventListener('load', startup, false);
 })();
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
 
 /*
 Copyright (c) 2011, Daniel Guerrero
