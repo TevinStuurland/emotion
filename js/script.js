b2fbb2d6fdc4639b0d3868dfe7b1b79bfd5b4598
mo <thisismoniac@gmail.com>
2017-10-19 00:22:22 +0200

    challenge

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var Base64Binary = {
 	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-	
+
 	/* will return a  Uint8Array type */
 	decodeArrayBuffer: function(input) {
 		var bytes = (input.length/4) * 3;
 		var ab = new ArrayBuffer(bytes);
 		this.decode(input, ab);
-		
+
 		return ab;
 	},
 
@@ -24,39 +24,57 @@ var Base64Binary = {
 		input = this.removePaddingChars(input);
 
 		var bytes = parseInt((input.length / 4) * 3, 10);
-		
+
 		var uarray;
 		var chr1, chr2, chr3;
 		var enc1, enc2, enc3, enc4;
 		var i = 0;
 		var j = 0;
-		
+
 		if (arrayBuffer)
 			uarray = new Uint8Array(arrayBuffer);
 		else
 			uarray = new Uint8Array(bytes);
-		
+
 		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-		
-		for (i=0; i<bytes; i+=3) {	
+
+		for (i=0; i<bytes; i+=3) {
 			//get the 3 octects in 4 ascii chars
 			enc1 = this._keyStr.indexOf(input.charAt(j++));
 			enc2 = this._keyStr.indexOf(input.charAt(j++));
 			enc3 = this._keyStr.indexOf(input.charAt(j++));
 			enc4 = this._keyStr.indexOf(input.charAt(j++));
-	
+
 			chr1 = (enc1 << 2) | (enc2 >> 4);
 			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
 			chr3 = ((enc3 & 3) << 6) | enc4;
-	
-			uarray[i] = chr1;			
+
+			uarray[i] = chr1;
 			if (enc3 != 64) uarray[i+1] = chr2;
 			if (enc4 != 64) uarray[i+2] = chr3;
 		}
-	
-		return uarray;	
+
+		return uarray;
 	}
 }
+
+var textArray = [
+    'anger',
+    'contempt',
+		'disgust',
+		'fear',
+		'happiness',
+		'neutral',
+		'sadness',
+		'surprise'
+];
+var randomNumber = Math.floor(Math.random()*textArray.length);
+
+function setChallenge () {
+	changeDoc = document.querySelector('.challenge ')
+}
+
+setChallenge()
 
 //apiKey: Replace this with your own Project Oxford Emotion API key, please do not use my key. I include it here so you can get up and running quickly but you can get your own key for free at https://www.projectoxford.ai/emotion
 var apiKey = "d6bc951e60d54b5a9180e63fecdce4af";
@@ -151,14 +169,14 @@ $("#response").text(data);
     video.addEventListener('canplay', function(ev){
       if (!streaming) {
         height = video.videoHeight / (video.videoWidth/width);
-      
+
         // Firefox currently has a bug where the height can't be read from
         // the video, so we will make assumptions if this happens.
-      
+
         if (isNaN(height)) {
           height = width / (4/3);
         }
-      
+
         video.setAttribute('width', width);
         video.setAttribute('height', height);
         canvas.setAttribute('width', width);
@@ -174,11 +192,11 @@ $("#response").text(data);
       // console.log(counter);
       ev.preventDefault();
     }, false);
-    
+
     clearphoto();
   }
 
-  
+
 
   // Fill the photo with an indication that none has been
   // captured.
@@ -192,8 +210,8 @@ $("#response").text(data);
     photo.setAttribute('src', data);
     photo2.setAttribute('src', data);
   }
-  
-  
+
+
   // Capture a photo by fetching the current contents of the video
   // and drawing it into a canvas, then converting that to a PNG
   // format data URL. By drawing it on an offscreen canvas and then
@@ -202,35 +220,35 @@ $("#response").text(data);
 
   function takepicture() {
     var context = canvas.getContext('2d');
-    
+
     if (width && height) {
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
-    
+
       var data = canvas.toDataURL('image/png'),
       test = dataURItoBlob( data )
 
          function snapPost () {
             let file = document.getElementById('photo').src.substring(23).replace(' ', '+');
             let Img = Base64Binary.decodeArrayBuffer(file);
-            let ajax = new XMLHttpRequest(); 
+            let ajax = new XMLHttpRequest();
             console.log('Photo 1 in case');
-            CallAPI(test, apiUrl, apiKey)       
+            CallAPI(test, apiUrl, apiKey)
            }
              function snapPost2 () {
             let file = document.getElementById('photo-2').src.substring(23).replace(' ', '+');
             let Img = Base64Binary.decodeArrayBuffer(file);
-            let ajax = new XMLHttpRequest(); 
+            let ajax = new XMLHttpRequest();
             console.log('Photo 1 in case');
-            CallAPI(test, apiUrl, apiKey)       
+            CallAPI(test, apiUrl, apiKey)
            }
-      
+
       switch (count % 2) {
         case 0:
         photo.setAttribute('src', data);
           console.log('Photo 1');
-          snapPost() 
+          snapPost()
           break;
         case 1: // foo is 0 so criteria met here so this block will run
         photo2.setAttribute('src', data);
@@ -239,8 +257,8 @@ $("#response").text(data);
           // NOTE: the forgotten break would have been here
       };
       console.log(count);
-      
-      
+
+
     } else {
       clearphoto();
     }
@@ -272,39 +290,3 @@ function dataURItoBlob(dataURI) {
 
     return new Blob([ia], {type:mimeString});
 }
-
-/*
-Copyright (c) 2011, Daniel Guerrero
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL DANIEL GUERRERO BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
- * Uses the new array typed in javascript to binary base64 encode/decode
- * at the moment just decodes a binary base64 encoded
- * into either an ArrayBuffer (decodeArrayBuffer)
- * or into an Uint8Array (decode)
- * 
- * References:
- * https://developer.mozilla.org/en/JavaScript_typed_arrays/ArrayBuffer
- * https://developer.mozilla.org/en/JavaScript_typed_arrays/Uint8Array
- */
-
-
-
